a7dc41db5687d0e82494782f0ff3d1c95d79c2e2
“anj0la” <anjola.aina@gmail.com>
2024-05-29 12:52:50 -0600

    created test file for preprocess data. need to fix error in it that doesnt remove punctuation.

---
+++ tests/test_preprocess_data.py
@@ -0,0 +1,15 @@
+"""
+File: test_preprocess_data.py
+
+Author: Anjola Aina
+Date Modified: May 29th, 2024
+
+Description:
+    This file contains all the necessary functions to test the preprocessing data functionality, which aims to "clean" the corpus
+    by removing irrelevant symbols and text that may affect the classification results.
+    
+    Currently, the test file has a coverage of ____%.
+
+Functions:
+    TBA
+"""
