4d41882da1a51e12f4fef625510b402408986225
“anj0la” <anjola.aina@gmail.com>
2024-05-30 21:36:31 -0600

    updated reddit_scraper to be a class

--- utils/get_labels.py
+++ utils/get_labels.py
@@ -6,7 +6,7 @@ Source: to help label the data to be positive, netural or negative: https://medi
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from constants.test_constants import TEST_CORPUS
 
-def get_vader_sentiment(sentence):
+def _get_vader_sentiment(sentence):
     analyzer = SentimentIntensityAnalyzer()
     scores = analyzer.polarity_scores(sentence)
     if scores['compound'] >= 0.05:
@@ -19,7 +19,7 @@ def get_vader_sentiment(sentence):
 def define_labels(raw_corpus):
     labels = []
     for sentence in raw_corpus:
-        sentiment = get_vader_sentiment(sentence)
+        sentiment = _get_vader_sentiment(sentence)
         labels.append(sentiment)
     return labels
             
