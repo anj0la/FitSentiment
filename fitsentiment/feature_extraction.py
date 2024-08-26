4a50c695343fbd7196e1ac5215a25ae6cd0a496c
anj0la <anjola.aina@gmail.com>
2024-08-26 15:13:29 -0600

    created feature extraction module

---
+++ fitsentiment/feature_extraction.py
@@ -0,0 +1,12 @@
+"""
+File: feature_extraction.py
+
+Author: Anjola Aina
+Date Modified: August 26th, 2024
+
+This file contains all the necessary functions used to extract features from the collected data.
+It is assumed that when the functions below are used, that the data has been cleaned and stopwords have been removed.
+
+Functions:
+    TODO: Populate this section with functions.
+"""
