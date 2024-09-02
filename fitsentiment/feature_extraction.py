dc0458595ec824f5669eda12c1b152eba1d20fd9
anj0la <anjola.aina@gmail.com>
2024-09-02 09:43:58 -0600

    finished documenting two funcs, and updated annotations json file

--- fitsentiment/feature_extraction.py
+++ fitsentiment/feature_extraction.py
@@ -15,54 +15,32 @@ Functions:
 """
 import pandas as pd
 import spacy
-from spacy.tokens import DocBin
+from spacy.tokens import DocBin, Doc
 from tqdm import tqdm
 
-def get_spacy_doc(log_file_path: str, data):
+def extract_entities(path: str, text: str, doc: Doc, annot: str) -> list:
     """
-    This function creates spaCy DocBin onjects from annotated data.
-    
-    It prepares data for training a custom NER model by transforming annotated data
-    into a format compatible with spaCy v3, creating Doc objects with character spans linked
-    to entities.
+    This function extracts entities from the annonations.
     
     Entity overlap is prevented to avoid training issues.
     
     Data issues are logged and captured in the log_file to aid with debugging and data quality assessment.
 
+
     Args:
-        log_file_path (_type_): _description_
-        data (_type_): _description_
+        path (str): The path to the log file. 
+        text (str): The text to process.
+        doc (Doc): The text as a Doc object.
+        annot (str): The list of annotations.
 
     Returns:
-        _type_: _description_
+        list: The extracted entities from the text.
     """
-    # create a blank spaCy pipeline
-    nlp = spacy.blank('en')
-    db = DocBin()
-    
-    # iterate through the data
-    for text, annot in tqdm(data):
-        doc = nlp.make_doc(text) # create doc object from the text
-        annot = annot['entities']
-        
-        # extract entities from the annotations
-        ents = extract_entities(log_file_path, text, doc, annot)
-        
-        # attempt to label the text with the entities and add it to the docbin object
-        try:
-            doc.ents = ents 
-            db.add(doc)
-        except:
-            # no entities were extracted from the text, don't add this entity to the docbin object
-            pass 
-        
-def extract_entities(log_file_path, text, doc, annot):
     ents = []
     ent_indices = []
     
     # open the log file for writing
-    f = open(file=log_file_path, mode='w') 
+    f = open(file=path, mode='w') 
     
     for start, end, label in annot:
         # check if the current entity overlaps with any previously processed entity
@@ -90,3 +68,43 @@ def extract_entities(log_file_path, text, doc, annot):
     f.close()
     
     return ents
+
+def get_spacy_doc(path: str, data: str) -> DocBin:
+    """
+    This function creates spaCy DocBin onjects from annotated data.
+    
+    It prepares data for training a custom NER model by transforming annotated data
+    into a format compatible with spaCy v3, creating Doc objects with character spans linked
+    to entities.
+    
+    Args:
+        path (str): The path to the log file.
+        data (str): The converted json file, containing the list of annotations.
+
+    Returns:
+        DocBin: The DocBin object, consisting of Doc objects with character spans linked to each entity.
+    """
+    # create a blank spaCy pipeline
+    nlp = spacy.blank('en')
+    db = DocBin()
+    
+    # iterate through the data
+    for text, annot in tqdm(data):
+        doc = nlp.make_doc(text) # create doc object from the text
+        annot = annot['entities']
+        
+        # extract entities from the annotations
+        ents = extract_entities(path, text, doc, annot)
+        
+        # attempt to label the text with the entities and add it to the docbin object
+        try:
+            doc.ents = ents 
+            db.add(doc)
+        except:
+            # no entities were extracted from the text, don't add this entity to the docbin object
+            pass 
+        
+    return db
+
+def prepare_data_for_training(json_file: str) -> None:
+    pass
